d790a6df3c2672fad46af2b1bed9a4348ca11ab7
Andrea Basile <andrea.basile@apuliasoft.com>
2022-07-17 16:56:27 +0200

    Fix impaginazione

--- lib/screens/admin_panel.dart
+++ lib/screens/admin_panel.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:ombrelloni_flutter/models/nav_drawer.dart';
 
 class AdminPanel extends StatefulWidget {
   const AdminPanel({Key? key}) : super(key: key);
@@ -8,24 +7,71 @@ class AdminPanel extends StatefulWidget {
 }
 
 class _AdminPanel extends State<AdminPanel> {
-  int _selectedIndex = 0;
-
-  void changeIndex(int i) {
-    setState(() {
-      _selectedIndex = i;
-    });
-  }
-
+  String text = "Initial Text";
+  List<Widget> widgetList = [
+    Scaffold(backgroundColor: Colors.amber),
+    Scaffold(backgroundColor: Colors.blue),
+    Scaffold(backgroundColor: Colors.red),
+    Scaffold(backgroundColor: Colors.green)
+  ];
+  int widgetIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(_selectedIndex.toString()),
-      ),
-      drawer: NavDrawer(),
-      appBar: AppBar(
-        title: const Text('Side menu'),
-      ),
-    );
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              const DrawerHeader(
+                child: Text(
+                  'Gestione',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: AssetImage('images/beachmenu.jpg'))),
+              ),
+              Column(children: <Widget>[
+                ListTile(
+                    leading: const Icon(Icons.edit_calendar),
+                    title: const Text('Calendario'),
+                    onTap: () {
+                      setState(() {
+                        widgetIndex = 0;
+                      });
+                    }),
+                ListTile(
+                    leading: const Icon(Icons.book_online),
+                    title: const Text('Prenotazioni'),
+                    onTap: () {
+                      setState(() {
+                        widgetIndex = 1;
+                      });
+                    }),
+                ListTile(
+                    leading: const Icon(Icons.map),
+                    title: const Text('Mappa spiaggia'),
+                    onTap: () {
+                      setState(() {
+                        widgetIndex = 2;
+                      });
+                    }),
+                ListTile(
+                    leading: const Icon(Icons.edit),
+                    title: const Text('Modifica mappa'),
+                    onTap: () {
+                      setState(() {
+                        widgetIndex = 3;
+                      });
+                    }),
+              ])
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: const Text("Gestione Spiaggia"),
+        ),
+        body: widgetList[widgetIndex]);
   }
 }
